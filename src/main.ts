27354360715ee28bf3bc0a488282809273d2694e
Hernan-Alonso-Calderon <hernanalonsocalderon@gmail.com>
2025-01-17 04:05:43 -0500

    feat: add accessibility

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
-const accordionItems = document.querySelectorAll(".accordion__item");
+document.addEventListener("DOMContentLoaded", () => {
+  const accordionItems: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".accordion__item");
 
-accordionItems.forEach((item) => {
-  const header = item.querySelector(".accordion__title") as HTMLElement;
-  const content = item.querySelector(".accordion__content") as HTMLDivElement;
+  accordionItems.forEach((item) => {
+    const header = item.querySelector(".accordion__title") as HTMLElement;
+    const content = item.querySelector(".accordion__content") as HTMLDivElement;
 
-  document.addEventListener("DOMContentLoaded", () => {
     header.addEventListener("click", () => {
       const isOpen = header.classList.contains("active");
 
-      // Close all open accordion items
       accordionItems.forEach((otherItem) => {
         const otherHeader = otherItem.querySelector(".accordion__title")!;
         const otherContent = otherItem.querySelector(
@@ -18,7 +18,6 @@ accordionItems.forEach((item) => {
         otherContent.style.maxHeight = "0";
       });
 
-      // Open the clicked accordion item if it wasn't already open
       if (!isOpen) {
         header.classList.add("active");
         content.style.maxHeight = `${content.scrollHeight}px`;
